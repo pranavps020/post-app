c57357b72558ab8e55b6b4521560a90bd004bd6e
Pranav P.S <pranavps020@gmail.com>
2021-01-05 14:50:14 +0530

    Load more button implemented

--- src/Components/Posts/Posts.jsx
+++ src/Components/Posts/Posts.jsx
@@ -1,11 +1,12 @@
-import {React} from 'react';
-import {Grid} from '@material-ui/core';
+import {React,useState} from 'react';
+import {Grid,Button,Container} from '@material-ui/core';
 import Post from './Post';
 import {useSelector, useDispatch} from 'react-redux';
 import {deletePost} from '../../Actions/Posts';
 
+
 function Posts() {
-    
+   const [Visible, setVisible] = useState(4)    
    const PostsCollection = useSelector((state) => state.posts )
    const dispatch = useDispatch();
 
@@ -13,14 +14,18 @@ function Posts() {
         dispatch(deletePost(id));
     }
     
+    const LoadMore = () =>{
+        setVisible((prevValue)=>prevValue+4)
+    }
     return (
         <div>
         <Grid  container justify="center" spacing={4}>
-        {PostsCollection.map((post) => (
+        {PostsCollection.slice(0,Visible).map((post) => (
             <Grid key={post.id} item  xs={12} sm={6} md={4} lg={3}> 
              <Post handleDelete={handleDelete} id={post.id} title={post.title} body={post.body}/>
             </Grid>))}
         </Grid>
+        <Button style={{margin:'0 auto',display:'block'}} onClick={LoadMore} >Load more</Button>
         </div>
     )
 }
